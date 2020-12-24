dd94296da8b3a8b07b96c0d16e664ca31827135f
CodeKing12 <eyetukingsley330@gmail.com>
2020-12-24 21:38:38 +0100

    Add files via upload

---
+++ Unit.py
@@ -0,0 +1,419 @@
+word = input("What would you like me to beautify?: ")
+index = 0
+for character in word:
+    if character == "A" or character == "a":
+        print("""              AAAAAAAAAAA
+             AAAAAAAAAAAAA
+            AAAAA     AAAAA
+           AAAAA       AAAAA
+          AAAAAAAAAAAAAAAAAAA
+         AAAAAAAAAAAAAAAAAAAAA
+        AAAAA             AAAAA
+       AAAAA               AAAAA
+      AAAAA                 AAAAA
+     AAAAA                   AAAAA
+     """)
+
+    if character == "B" or character == "b":
+        print("""     BBBBBBBBBBBBBBBBB
+     BBB             BB
+     BBB              BB
+     BBB             BB
+     BBBBBBBBBBBBBBBB
+     BBB             BB
+     BBB              BB
+     BBB             BB
+     BBBBBBBBBBBBBBBB
+                 """)
+    if character == "C" or character == "c":
+        print("""    CCCCCCCCCCCCCCCCCCCCCC
+    CCCCCCCCCCCCCCCCCCCCCC
+    CCCC    
+    CCCC   
+    CCCC  
+    CCCC 
+    CCCC
+    CCCC
+    CCCC
+    CCCC
+    CCCC
+    CCCC
+    CCCC
+    CCCC
+    CCCCCCCCCCCCCCCCCCCCCC
+    CCCCCCCCCCCCCCCCCCCCCC
+                 """)
+    if character == "D" or character == "d":
+        print("""    DDDDDDDDDDDDDDDDDDDDDDDDD
+    DDDDDDDDDDDDDDDDDDDDDDDDDD
+    DDDD                    DDDD
+    DDDD                     DDDD
+    DDDD                      DDDD
+    DDDD                      DDDD
+    DDDD                      DDDD
+    DDDD                      DDDD
+    DDDD                      DDDD
+    DDDD                     DDDD
+    DDDD                    DDDD
+    DDDD                   DDDD
+    DDDDDDDDDDDDDDDDDDDDDDDDD
+    DDDDDDDDDDDDDDDDDDDDDDDD
+                     """)
+    if character == "E" or character == "e":
+        print("""    EEEEEEEEEEEEEEEEEEEE
+    EEEEEEEEEEEEEEEEEEEE
+    EEEE
+    EEEE
+    EEEE
+    EEEE
+    EEEEEEEEEEEEEEEEEEEE
+    EEEEEEEEEEEEEEEEEEEE
+    EEEE
+    EEEE
+    EEEE
+    EEEEEEEEEEEEEEEEEEEE
+    EEEEEEEEEEEEEEEEEEEE
+                         """)
+    if character == "F" or character == "f":
+        print("""    FFFFFFFFFFFFFFFFF
+    FFFFFFFFFFFFFFFFF
+    FFFF
+    FFFF
+    FFFF
+    FFFFFFFFFFFFFFFFF
+    FFFFFFFFFFFFFFFFF
+    FFFF
+    FFFF
+    FFFF
+    FFFF
+    FFFF
+    FFFF
+                     """)
+    if character == "G" or character == "g":
+        print("""    GGGGGGGGGGGGGGGGGGGGGGGGGGGG
+    GGGGGGGGGGGGGGGGGGGGGGGGGGGG
+    GGGG
+    GGGG
+    GGGG
+    GGGG
+    GGGG                    
+    GGGG              GGGGGGGGGGGGGGG
+    GGGG                    GGGG
+    GGGG                    GGGG
+    GGGG                    GGGG
+    GGGGGGGGGGGGGGGGGGGGGGGGGGGG
+    GGGGGGGGGGGGGGGGGGGGGGGGGGGG
+                     """)
+    if character == "H" or character == "h":
+        print("""    HHHHH                      HHHHH
+    HHHHH                      HHHHH
+    HHHHH                      HHHHH
+    HHHHH                      HHHHH
+    HHHHH                      HHHHH
+    HHHHH                      HHHHH
+    HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+    HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+    HHHHH                      HHHHH
+    HHHHH                      HHHHH
+    HHHHH                      HHHHH
+    HHHHH                      HHHHH
+    HHHHH                      HHHHH
+    HHHHH                      HHHHH
+                 """)
+    if character == "I" or character == "i":
+        print("""    IIIIIIIIIIIIIIIIIIIIIIIII
+    IIIIIIIIIIIIIIIIIIIIIIIII          
+              IIIII          
+              IIIII          
+              IIIII          
+              IIIII          
+              IIIII          
+              IIIII          
+              IIIII          
+              IIIII          
+    IIIIIIIIIIIIIIIIIIIIIIIII          
+    IIIIIIIIIIIIIIIIIIIIIIIII
+                """)
+    if character == "J" or character == "j":
+        print("""    JJJJJJJJJJJJJJJJJJJJJJJ
+    JJJJJJJJJJJJJJJJJJJJJJJ
+               JJJJJ
+               JJJJJ
+               JJJJJ
+               JJJJJ
+               JJJJJ
+               JJJJJ
+               JJJJJ
+               JJJJJ
+               JJJJJ
+               JJJJJ
+JJJJJJJJJJJJJJJJJJJJ
+JJJJJJJJJJJJJJJJJJJJ 
+                 """)
+    if character == "K" or character == "k":
+        print("""    KKKK             KKKK
+    KKKK           KKKK
+    KKKK         KKKK
+    KKKK       KKKK
+    KKKK     KKKK
+    KKKKKKKKKKK
+    KKKK     KKKK
+    KKKK       KKKK
+    KKKK         KKKK
+    KKKK           KKKK
+    KKKK             KKKK
+    KKKK               KKKK
+                 """)
+    if character == "L" or character == "l":
+        print("""    LLLL
+    LLLL
+    LLLL
+    LLLL
+    LLLL
+    LLLL
+    LLLL
+    LLLL
+    LLLL
+    LLLL
+    LLLL
+    LLLL
+    LLLLLLLLLLLLLLLLLLLLLLLLLL
+    LLLLLLLLLLLLLLLLLLLLLLLLLL
+                 """)
+    if character == "M" or character == "m":
+        print("""    MMMMMMMMM                 MMMMMMMM
+    MMMM   MMMM             MMMM  MMMM
+    MMMM     MMMM         MMMM    MMMM
+    MMMM       MMMM     MMMM      MMMM
+    MMMM         MMMM MMMM        MMMM
+    MMMM             MM           MMMM
+    MMMM                          MMMM
+    MMMM                          MMMM
+    MMMM                          MMMM
+    MMMM                          MMMM
+    MMMM                          MMMM
+    MMMM                          MMMM
+    MMMM                          MMMM
+                 """)
+
+    if character == "N" or character == "n":
+        print("""
+    NNNNNNNN                    NNNN
+    NNNN  NNNN                  NNNN
+    NNNN    NNNN                NNNN
+    NNNN      NNNN              NNNN
+    NNNN        NNNN            NNNN
+    NNNN          NNNN          NNNN
+    NNNN            NNNN        NNNN
+    NNNN              NNNN      NNNN
+    NNNN                NNNN    NNNN
+    NNNN                  NNNN  NNNN
+    NNNN                    NNNNNNNN
+    """)
+    if character == "O" or character == "o":
+        print("""
+          OOOOOOOOOOOOOOOOOO         
+       OOOO                OOOO
+      OOOO                  OOOO
+     OOOO                    OOOO
+    OOOO                      OOOO
+    OOOO                      OOOO
+    OOOO                      OOOO
+    OOOO                      OOOO
+     OOOO                    OOOO
+      OOOO                  OOOO
+       OOOO                OOOO
+          OOOOOOOOOOOOOOOOOO
+    """)
+
+    if character == "P" or character == "p":
+        print("""
+    PPPPPPPPPPPPPPPPPP
+    PPPP            PPPP
+    PPPP             PPPP  
+    PPPP             PPPP
+    PPPP            PPPP
+    PPPPPPPPPPPPPPPPPP
+    PPPP
+    PPPP
+    PPPP
+    PPPP
+    PPPP
+    PPPP
+        """)
+
+    if character == "Q" or character == "q":
+        print("""
+          QQQQQQQQQQQQQQQQQQ         
+       QQQQ                QQQQ
+      QQQQ                  QQQQ
+     QQQQ                    QQQQ
+    QQQQ                      QQQQ
+    QQQQ                      QQQQ
+    QQQQ                      QQQQ
+    QQQQ                      QQQQ
+     QQQQ          QQQQ      QQQQ
+      QQQQ          QQQQ    QQQQ
+       QQQQ          QQQQ  QQQQ
+          QQQQQQQQQQQQQQQQQQ
+                       QQQQ
+                        QQQQ
+                         QQQQ
+        """)
+
+    if character == "R" or character == "r":
+        print("""
+    RRRRRRRRRRRRRRRRRR
+    RRRR            RRRR
+    RRRR             RRRR  
+    RRRR             RRRR
+    RRRR            RRRR
+    RRRRRRRRRRRRRRRRRR
+    RRRRRRRR
+    RRRR  RRRR
+    RRRR     RRRR
+    RRRR       RRRR
+    RRRR         RRRR
+    RRRR            RRRR
+        """)
+
+    if character == "S" or character == "s":
+        print("""
+         SSSSSSSSSSSS
+      SSSS
+    SSSS
+    SSSS
+    SSSS  
+      SSSS
+         SSSSSSSS
+               SSSS
+                SSSS
+                SSSS
+                SSSS
+               SSSS
+   SSSSSSSSSSSSS
+        """)
+
+    if character == "T" or character == "t":
+       print("""
+   TTTTTTTTTTTTTTTTTTTTTTTTT
+   TTTTTTTTTTTTTTTTTTTTTTTTT          
+            TTTTT          
+            TTTTT          
+            TTTTT          
+            TTTTT          
+            TTTTT          
+            TTTTT          
+            TTTTT          
+            TTTTT
+            TTTTT
+       """)
+
+    if character == "U" or character == "u":
+       print("""
+   UUUU                     UUUU
+   UUUU                     UUUU
+   UUUU                     UUUU
+   UUUU                     UUUU
+   UUUU                     UUUU
+   UUUU                     UUUU
+   UUUU                     UUUU
+   UUUU                     UUUU
+   UUUU                     UUUU
+   UUUU                     UUUU
+     UUUU                 UUUU
+       UUUU             UUUU
+        UUUUUUUUUUUUUUUUUUU
+       """)
+
+    if character == "V" or character == "v":
+        print("""
+  VVVV                     VVVV
+   VVVV                   VVVV
+    VVVV                 VVVV
+     VVVV               VVVV
+      VVVV             VVVV
+       VVVV           VVVV
+        VVVV         VVVV
+         VVVV       VVVV
+          VVVV     VVVV
+           VVVV   VVVV
+            VVVVVVVVV
+        """)
+
+    if character == "W" or character == "w":
+        print("""
+  WWWW                                          WWWW
+   WWWW                                        WWWW
+    WWWW                                      WWWW
+     WWWW                                    WWWW
+      WWWW             WWWWWWWWW            WWWW
+       WWWW           WWWW   WWWW          WWWW
+        WWWW         WWWW     WWWW        WWWW
+         WWWW       WWWW       WWWW      WWWW
+          WWWW     WWWW         WWWW    WWWW
+           WWWW   WWWW           WWWW  WWWW
+            WWWWWWWWW            WWWWWWWWW
+        """)
+
+    if character == "X" or character == "x":
+        print("""
+         XXXX       XXXX
+          XXXX     XXXX
+           XXXX   XXXX
+            XXXX XXXX
+             XXXXXXX
+              XXXXX
+               XXX
+             XXXXXXX
+            XXXX XXXX
+           XXXX   XXXX
+          XXXX     XXXX
+         XXXX       XXXX
+        XXXX         XXXX
+        """)
+
+    if character == "Y" or character == "y":
+        print("""
+       YYYY           YYYY
+        YYYY         YYYY
+         YYYY       YYYY
+          YYYY     YYYY
+           YYYY   YYYY
+            YYYYYYYYY
+             YYYYYYY
+              YYYYY
+              YYYYY
+              YYYYY
+              YYYYY
+              YYYYY
+              YYYYY
+    """)
+
+    if character == "Z" or character == "z":
+        print("""
+    ZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+    ZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+                           ZZZZ 
+                         ZZZZ
+                       ZZZZ
+                     ZZZZ
+                   ZZZZ
+                 ZZZZ
+               ZZZZ
+             ZZZZ
+           ZZZZ
+         ZZZZ
+       ZZZZ
+    ZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+    ZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+        """)
+
+    if character == " " or character == "  " or character == "-":
+        print("""              |
+              |
+              |
+              |
+              |
+              |
+              """)
